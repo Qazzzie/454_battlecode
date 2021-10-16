c55de7b9eb6eb25869667eb08231a6297cbe4480
Sam Little <kagutaba256@gmail.com>
2021-10-15 17:45:25 -0700

    adding basic enlightenment center logic, some utility functions

--- src/SaZaPraYi/EnlightenmentCenter.java
+++ src/SaZaPraYi/EnlightenmentCenter.java
@@ -1,27 +1,70 @@
 package SaZaPraYi;
 
-import battlecode.common.Direction;
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import battlecode.common.RobotType;
+import battlecode.common.*;
+
+import java.util.ArrayList;
 
 
 public class EnlightenmentCenter {
 
+    /*
+    DESIRED_SLANDERER_RATIO is the percentage of total units that
+    should be nearby slanderers. The EC can only see nearby units
+    40 units away, so it checks all of those, and if the number
+    of slanderers is less than this ratio, it spawns a new one.
+    This right now is 0.2f, or 20%. This may be a little high...
+     */
     static final double DESIRED_SLANDERER_RATIO = 0.2;
+
+    /*
+    POLITICIAN_SPAWN_PERCENTAGE is the percent chance a Politician
+    should be chosen to be spawned. Currently, it's 60%. If a Politician
+    isn't chosen, a Muckraker is chosen.
+     */
+    static final double POLITICIAN_SPAWN_PERCENTAGE = 0.6;
+
+    /*
+    This is the minimum number of slanderers that should be around
+    the EC. If there are less than 5 slanderers, it will make another
+    slanderer, UNLESS the EC senses that it is under attack
+    (because then it will spawn Politicians to defend)
+     */
     static final int MINIMUM_SLANDERERS = 5;
-    int MINIMUM_UNIT_INFLUENCE = 10;
-    int ROUNDS_BEFORE_START_VOTING = 300;
+
+    /*
+    This is the minimum amount of influence that the EC should
+    spend on a unit. This was chosen because according to the
+    rules, Politicians with less than 10 influence will just
+    explode without doing anything when they empower.
+     */
+    static final int MINIMUM_UNIT_INFLUENCE = 10;
+
+    /*
+    This is the number of rounds before the EC should start
+    voting. I just randomly chose this as 300, but it should
+    probably be adjusted (as well as getBidAmount function
+    itself). Before round 300, no voting is done.
+     */
+    static final int ROUNDS_BEFORE_START_VOTING = 50;
+
+    /*
+    The number of rounds we should wait before voting again.
+    When this is 5, vote every 5th round.
+     */
+    static final int VOTE_EVERY_N_ROUNDS = 5;
+
+    /*
+    When spawning a Politician to defend, add an additional
+    this many enemy unit average conviction costs to the total
+    influence to spend on the defender.
+    Think of this as a buffer that will cover the cost of
+    2 additional enemy units that come into radius of the
+    politician by the time it is ready to empower.
+     */
+    static final int DEFENDER_ADDITIONAL_UNIT_BUFFER = 2;
 
     private static RobotController rc;
     private static RobotUtils utils;
-
-    static final RobotType[] spawnableRobot = {
-            RobotType.POLITICIAN,
-            RobotType.SLANDERER,
-            RobotType.MUCKRAKER,
-    };
-
 
     /**
      * The constructor for the EnlightenmentCenter controller object.
@@ -40,14 +83,83 @@ public class EnlightenmentCenter {
      * @throws GameActionException if anything would cause one.
      */
     public void run() throws GameActionException {
+        // Build robots
         buildRobots();
-        bid();
+        // If it's a vote round, we vote
+        if(rc.getRoundNum() % VOTE_EVERY_N_ROUNDS == 0) bid();
     }
 
+    /**
+     * This contains the logic for actually building robots
+     *
+     * @throws GameActionException if anything would cause one
+     */
     private void buildRobots() throws GameActionException {
-        
+        Team playerTeam = rc.getTeam();
+        Team enemyTeam = playerTeam.opponent();
+
+        RobotType robotTypeToBuild;
+        int influenceToSpendOnUnit;
+
+        int senseRadius = rc.getType().sensorRadiusSquared;
+
+        // Initializing nearby robot lists
+        ArrayList<RobotInfo> nearbyAlliedRobots = new ArrayList<>();
+        ArrayList<RobotInfo> nearbyEnemyRobots = new ArrayList<>();
+        ArrayList<RobotInfo> nearbyGreyRobots = new ArrayList<>();
+
+        // Sort nearby robots into the above lists
+        for(RobotInfo robot : rc.senseNearbyRobots(senseRadius)) {
+            if(robot.getTeam() == playerTeam) nearbyAlliedRobots.add(robot);
+            else if(robot.getTeam() == enemyTeam) nearbyEnemyRobots.add(robot);
+            else nearbyGreyRobots.add(robot);
+        }
+
+        // If there's enemies nearby, spawn a Politician defender
+        if(nearbyEnemyRobots.size() > 0 || nearbyGreyRobots.size() > 0) {
+            robotTypeToBuild = RobotType.POLITICIAN;
+            int totalConvictionOfNearbyEnemyUnits = 0;
+            for(RobotInfo enemy : nearbyEnemyRobots) {
+                totalConvictionOfNearbyEnemyUnits += enemy.conviction;
+            }
+            int averageConviction = totalConvictionOfNearbyEnemyUnits / nearbyEnemyRobots.size();
+            influenceToSpendOnUnit =
+                        totalConvictionOfNearbyEnemyUnits + (averageConviction * DEFENDER_ADDITIONAL_UNIT_BUFFER);
+        // Otherwise, spawn units as normal
+        } else {
+            // If we don't have enough slanderers, spawn one
+            int currentNumberOfRobots = rc.getRobotCount();
+            int currentNumberOfNearbySlanderers = 0;
+            for(RobotInfo robot : nearbyAlliedRobots)
+                if(robot.getType() == RobotType.SLANDERER)
+                    currentNumberOfNearbySlanderers++;
+            double currentSlandererToTotalUnitRatio = (double) currentNumberOfNearbySlanderers / currentNumberOfRobots;
+            if(currentSlandererToTotalUnitRatio < DESIRED_SLANDERER_RATIO
+                    || currentNumberOfNearbySlanderers < MINIMUM_SLANDERERS) {
+                robotTypeToBuild = RobotType.SLANDERER;
+                influenceToSpendOnUnit = 100; //TODO: change this
+            // Otherwise, spawn a Politician or Muckraker
+            } else {
+                robotTypeToBuild = randomSpawnableRobotType();
+                influenceToSpendOnUnit = 100; //TODO: change this
+            }
+        }
+        // Actually spawn the robot
+        if(influenceToSpendOnUnit < MINIMUM_UNIT_INFLUENCE) return;
+        Direction directionToPlace = utils.getDirectionOfRandomAdjacentEmptyTile(rc.getLocation());
+        if(directionToPlace != null) {
+            if(rc.canBuildRobot(robotTypeToBuild, directionToPlace, influenceToSpendOnUnit)) {
+                rc.buildRobot(robotTypeToBuild, directionToPlace, influenceToSpendOnUnit);
+            }
+        }
     }
 
+
+    /**
+     * This contains the logic for actually making bids
+     *
+     * @throws GameActionException if anything would cause one
+     */
     private void bid() throws GameActionException {
         int currentInfluence = rc.getInfluence();
         int turnCount = rc.getRoundNum();
@@ -75,12 +187,14 @@ public class EnlightenmentCenter {
     }
 
     /**
-     * Returns a random spawnable RobotType
+     * Returns either a Politician or Muckraker based on static ratios above.
      *
      * @return a random RobotType
      */
     static RobotType randomSpawnableRobotType() {
-        return spawnableRobot[(int) (Math.random() * spawnableRobot.length)];
+        double randomValue = Math.random();
+        if(randomValue < POLITICIAN_SPAWN_PERCENTAGE) return RobotType.POLITICIAN;
+        return RobotType.MUCKRAKER;
     }
 
 }
--- src/SaZaPraYi/RobotUtils.java
+++ src/SaZaPraYi/RobotUtils.java
@@ -1,8 +1,9 @@
 package SaZaPraYi;
 
-import battlecode.common.Direction;
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import battlecode.common.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class RobotUtils {
 
@@ -56,10 +57,40 @@ public class RobotUtils {
      * @throws GameActionException
      */
     boolean tryMove(Direction dir) throws GameActionException {
-        System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
+        //System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
         if (rc.canMove(dir)) {
             rc.move(dir);
             return true;
         } else return false;
+    }
+
+    /**
+     * Returns the direction of a random adjacent empty tile if there is one,
+     * otherwise returns null.
+     *
+     * @param tile The MapLocation to check the adjacent tiles of
+     * @return Either a direction towards a nearby empty tile, or null
+     * @throws GameActionException if anything in here would cause one
+     */
+    public Direction getDirectionOfRandomAdjacentEmptyTile(MapLocation tile) throws GameActionException {
+        ArrayList<MapLocation> nearbyAdjacentTiles = new ArrayList<>();
+        // First, put all the adjacent nearby tiles into a list...
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                if(j == 0 && i == 0) continue; // This would be the tile itself...
+                MapLocation temp = new MapLocation(tile.x + i, tile.y + j);
+                if (!rc.onTheMap(temp)) continue;
+                if (rc.senseRobotAtLocation(temp) == null)
+                    nearbyAdjacentTiles.add(temp);
+            }
+        }
+        if(nearbyAdjacentTiles.size() > 0) {
+            // Shuffle them...
+            Collections.shuffle(nearbyAdjacentTiles);
+            // And return a random one
+            return rc.getLocation().directionTo(nearbyAdjacentTiles.get(0));
+        }
+        // If there isn't any nearby tiles, return null
+        return null;
     }
 }
