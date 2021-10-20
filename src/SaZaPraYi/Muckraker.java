106194629c11e2daf30fb053af863dcde1f063db
yi-xuan-97 <56617127+yi-xuan-97@users.noreply.github.com>
2021-10-20 10:27:11 -0700

    exposing slanderer and avoid politician

--- src/SaZaPraYi/Muckraker.java
+++ src/SaZaPraYi/Muckraker.java
@@ -1,9 +1,6 @@
 package SaZaPraYi;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import battlecode.common.RobotInfo;
-import battlecode.common.Team;
+import battlecode.common.*;
 
 public class Muckraker {
     private static RobotController rc;
@@ -26,18 +23,45 @@ public class Muckraker {
      * @throws GameActionException if anything would cause one.
      */
     public void run() throws GameActionException {
+
         Team enemy = rc.getTeam().opponent();
         int actionRadius = rc.getType().actionRadiusSquared;
         for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
             if (robot.type.canBeExposed()) {
-                // It's a slanderer... go get them!
+
+                // It's a slanderer... go get them
                 if (rc.canExpose(robot.location)) {
                     System.out.println("e x p o s e d");
                     rc.expose(robot.location);
+
+                    //After exposing one slanderer, friend speech get conviction
+                    double multi_factor = (1+0.001* robot.influence);
+                    int initroundnum = rc.getRoundNum();
+                    Team friend = rc.getTeam();
+                    while ( (rc.getRoundNum()-initroundnum) <50 ){
+                        for (RobotInfo bot: rc.senseNearbyRobots(actionRadius,friend)){
+                            if(bot.getType()== RobotType.POLITICIAN){
+                                //Multiplicative factor to totally convince
+                                System.out.println("Mutiplicatived");
+                            }
+                        }
+                    }
+
                     return;
                 }
             }
         }
+
+        //Avoid politician
+        for (RobotInfo robot: rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, enemy)){
+            if(robot.getType()== RobotType.POLITICIAN) {
+                Direction enemy_loc = rc.getLocation().directionTo(robot.location);
+                if (utils.tryMove(enemy_loc.opposite())) {
+                    System.out.println("Moving away from ");
+                }
+            }
+        }
+
         if (utils.tryMove(utils.randomDirection()))
             System.out.println("I moved!");
     }
