4c00bb60513f1a6eef1d658f870276ac3fcef752
Sam Little <kagutaba256@gmail.com>
2021-10-20 18:08:08 -0700

    grey EC follow logic, some utils, flags, movement

--- src/SaZaPraYi/Muckraker.java
+++ src/SaZaPraYi/Muckraker.java
@@ -6,6 +6,17 @@ public class Muckraker {
     private static RobotController rc;
     private static RobotUtils utils;
 
+    private static MapLocation locationOfBase;
+    private static MapLocation locationOfEC;
+    private static boolean goingToBase;
+    private static int turnOfCooldown;
+
+    private static final int NUMBER_OF_POLITICIANS_NEEDED_BEFORE_RETURN_TO_EC = 3;
+    private static final int DISTANCE_BEFORE_SWITCHING_DIRECTIONS = 4;
+    private static final int POLITICIAN_CONSIDERED_CLOSE_DISTANCE = 5;
+    private static final int EC_COOLDOWN = 100;
+    private static final double EC_DEFLAG_PERCENT_CHANCE = 0.5;
+
     /**
      * The constructor for the Muckraker controller object.
      *
@@ -25,7 +36,16 @@ public class Muckraker {
     public void run() throws GameActionException {
 
         Team enemy = rc.getTeam().opponent();
+        int senseRadius = rc.getType().sensorRadiusSquared;
         int actionRadius = rc.getType().actionRadiusSquared;
+
+        // If we don't have a home base location, we probably just got spawned,
+        // and there is probably one nearby, so let's set it.
+        if(locationOfBase == null)
+            for(RobotInfo robot : rc.senseNearbyRobots(senseRadius))
+                if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER)
+                    locationOfBase = robot.getLocation();
+
         for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
             if (robot.type.canBeExposed()) {
 
@@ -52,6 +72,11 @@ public class Muckraker {
             }
         }
 
+        if(rc.getFlag(rc.getID()) != RobotUtils.flags.MUCKRAKER_FOUND_GREY_EC.ordinal())
+            utils.moveAwayFromOtherUnits();
+        boolean shouldReturn = handleGreyECFollow();
+        if(shouldReturn) return;
+
         //Avoid politician
         for (RobotInfo robot: rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, enemy)){
             if(robot.getType()== RobotType.POLITICIAN) {
@@ -64,6 +89,68 @@ public class Muckraker {
 
         if (utils.tryMove(utils.randomDirection()))
             System.out.println("I moved!");
+    }
+
+    private boolean handleGreyECFollow() throws GameActionException {
+        Team player = rc.getTeam();
+        int senseRadius = rc.getType().sensorRadiusSquared;
+
+        int currentFlag = rc.getFlag(rc.getID());
+        if(currentFlag == RobotUtils.flags.MUCKRAKER_FOUND_GREY_EC.ordinal()) {
+            int numberOfNearbyFriendlyPoliticians = 0;
+            for(RobotInfo robot : rc.senseNearbyRobots(senseRadius, player)) {
+                if(robot.getType() == RobotType.POLITICIAN)
+                    if(rc.getLocation().distanceSquaredTo(robot.getLocation()) < POLITICIAN_CONSIDERED_CLOSE_DISTANCE)
+                        numberOfNearbyFriendlyPoliticians++;
+                if(robot.getType() == RobotType.MUCKRAKER
+                        && rc.getFlag(robot.getID()) == RobotUtils.flags.MUCKRAKER_FOUND_GREY_EC.ordinal()) {
+                    if(Math.random() < EC_DEFLAG_PERCENT_CHANCE) {
+                        // cooldown flag
+                        rc.setFlag(RobotUtils.flags.MUCKRAKER_EC_COOLDOWN.ordinal());
+                        turnOfCooldown = rc.getRoundNum();
+                        return false;
+                    }
+                }
+            }
+            if(numberOfNearbyFriendlyPoliticians > NUMBER_OF_POLITICIANS_NEEDED_BEFORE_RETURN_TO_EC) {
+                goingToBase = false;
+            }
+            Direction toMove;
+            if(goingToBase) {
+                if(rc.getLocation().distanceSquaredTo(locationOfBase) < DISTANCE_BEFORE_SWITCHING_DIRECTIONS)
+                    goingToBase = false;
+                toMove = rc.getLocation().directionTo(locationOfBase);
+            } else {
+                if(rc.getLocation().distanceSquaredTo(locationOfEC) < senseRadius
+                        && rc.senseRobotAtLocation(locationOfEC).getTeam() == player) {
+                    rc.setFlag(RobotUtils.flags.NOTHING.ordinal());
+                    return true;
+                }
+                if(rc.getLocation().distanceSquaredTo(locationOfEC) < DISTANCE_BEFORE_SWITCHING_DIRECTIONS)
+                    goingToBase = true;
+                toMove = rc.getLocation().directionTo(locationOfEC);
+            }
+            utils.tryMove(toMove);
+            return true;
+        }
+
+        if(currentFlag == RobotUtils.flags.MUCKRAKER_EC_COOLDOWN.ordinal()) {
+            if(rc.getRoundNum() > turnOfCooldown + EC_COOLDOWN)
+                rc.setFlag(RobotUtils.flags.NOTHING.ordinal());
+        }
+
+        if(currentFlag == RobotUtils.flags.NOTHING.ordinal()) {
+            for(RobotInfo robot : rc.senseNearbyRobots(senseRadius)) {
+                if(robot.getTeam() == player) continue;
+                if(robot.getType() == RobotType.ENLIGHTENMENT_CENTER) {
+                    locationOfEC = robot.getLocation();
+                    rc.setFlag(RobotUtils.flags.MUCKRAKER_FOUND_GREY_EC.ordinal());
+                    goingToBase = true;
+                }
+            }
+
+        }
+        return false;
     }
 
 }
