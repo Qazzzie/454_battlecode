2ca41212e70ebc7c41977fad8d591ad27d8ac809
Sam Little <kagutaba256@gmail.com>
2021-11-01 16:42:23 -0700

    adding very basic EC bid function unit tests

---
+++ test/SaZaPraYi/EnlightenmentCenterTest.java
@@ -0,0 +1,36 @@
+package SaZaPraYi;
+
+import battlecode.common.RobotController;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.junit.Assert.*;
+
+public class EnlightenmentCenterTest {
+
+    private RobotController rc;
+    private RobotUtils utils;
+    private EnlightenmentCenter ec;
+
+
+    private void setupTests() {
+        rc = Mockito.mock(RobotController.class);
+        utils = new RobotUtils(rc);
+        ec = new EnlightenmentCenter(rc, utils);
+    }
+
+    @Test
+    public void testBidRatioIsGreaterThanZeroAfterTurn1000() {
+        setupTests();
+        int fakeTurnNumber = 1000;
+        double result = ec.getBidRatio(fakeTurnNumber);
+        assertTrue(result > 0);
+    }
+
+    @Test
+    public void testBidRatioIsLessThan1AfterTurn1000() {
+        int fakeTurnNumber = 1000;
+        double result = ec.getBidRatio(fakeTurnNumber);
+        assertTrue(result < 1);
+    }
+
+}
