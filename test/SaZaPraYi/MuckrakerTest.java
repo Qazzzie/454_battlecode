9c62f9569689dedc482e4566c30abb6076ebdb8f
Qazzzie <santangelozach@gmail.com>
2021-11-17 22:40:12 -0800

    mucktest minor change

--- test/SaZaPraYi/MuckrakerTest.java
+++ test/SaZaPraYi/MuckrakerTest.java
@@ -22,7 +22,6 @@ import org.junit.Test;
         public void exposeUnits() throws GameActionException{
             setupTests();
 
-
             setupTests();
             Team player = Team.A;
             Mockito.when(rc.getTeam()).thenReturn(player);
@@ -35,28 +34,24 @@ import org.junit.Test;
                     10,
                     new MapLocation(0, 3));
             RobotInfo unitB = new RobotInfo(4,
-            rc.getTeam().opponent(),
+                    rc.getTeam().opponent(),
                     RobotType.SLANDERER,
-            10,
-            10,
+                    10,
+                    10,
                     new MapLocation(2, -2));
-
-
-            Mockito.when(rc.getFlag(unitB.getID())).thenReturn(RobotUtils.flags.MUCKRAKER_FOUND_ENEMY_EC.ordinal());
 
             Mockito.when(rc.senseRobotAtLocation(unitA.location)).thenReturn(unitA);
             Mockito.when(rc.senseRobotAtLocation(unitB.location)).thenReturn(unitB);
-
-            Mockito.when(rc.canMove(Mockito.any())).thenReturn(true);
             MapLocation location = new MapLocation(0,0);
-            Mockito.when(rc.getLocation()).thenReturn(location);
             RobotInfo[] nearbyUnits = new RobotInfo[]{unitA, unitB};
+            int senseRadius = rc.getType().sensorRadiusSquared;
 
-            Mockito.when(rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, rc.getTeam())).thenReturn(nearbyUnits);
-            boolean moved = M.followEnemyECFlaggingMuckrakers();
-            assertTrue(moved);
+            Mockito.when(rc.senseNearbyRobots(rc.getType().actionRadiusSquared, rc.getTeam().opponent())).thenReturn(nearbyUnits);
+            Mockito.when(rc.canExpose(unitB.location)).thenReturn(true);
+//        Mockito.when(rc.expose(unitB.location)).then(true);
+            boolean exposed = M.exposeUnits();
+            assertTrue(exposed);
         }
-
 
         @Test
         public void followEnemyECFlaggingMuckrakers() throws GameActionException{
